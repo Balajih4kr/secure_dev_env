775a0906e7d209902c996f1365cbb7f964806302
BALAJIH4KR <rajanbala39@gmail.com>
2026-01-03 15:00:51 +0530

    crud with db fix

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI, Request, Form, Depends
-from fastapi.responses import HTMLResponse
+from wg.utils import assign_ip
+from fastapi.responses import HTMLResponse, RedirectResponse
 #from fastapi.staticfiles import staticfiles
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
@@ -9,14 +10,11 @@ from db import models
 
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
 
 templates = Jinja2Templates(directory="templates")
 
-peers_db = [
-    {"name": "Office-Laptop", "status": "Connected", "ip": "10.0.0.2", "usage": "1.2 GB"},
-    {"name": "Home-PC", "status": "Offline", "ip": "10.0.0.3", "usage": "450 MB"},
-    {"name": "Balajih4kr", "status": "Connected", "ip": "10.0.0.4", "usage": "451 MB"}
-]
+
 
 
 @app.post("/peers/create")
@@ -26,33 +24,38 @@ async def create_peer(
     public_key: str = Form(...),
     db: Session = Depends(get_db)
 ):
-    # 1. (Optional) Simple logic to pick the next IP
-    # In a real app, you'd calculate the next available IP in the 10.0.0.x range
-    assigned_ip = "10.0.0.15/32" 
 
-    # 2. Create the Database Record
-    new_peer = Peer(
+
+    
+    assigned_ip = assign_ip(db)
+    
+    if assigned_ip is None:
+        
+        return {"error": "No available IP addresses in the subnet."}
+
+    new_peer = models.Peer(
         developer_name=developer_name,
         device_name=device_name,
         public_key=public_key.strip(),
-        assigned_ip=assigned_ip,
+        assigned_ip=f"{assigned_ip}/32", 
         status="Offline"
     )
-    
+
     db.add(new_peer)
     db.commit()
     db.refresh(new_peer)
 
-    # 3. Redirect back to the dashboard to see the new card
+    
     return RedirectResponse(url="/", status_code=303)
 
 
 
 @app.get('/', response_class = HTMLResponse)
-async def dashboard(request: Request):
+async def dashboard(request: Request, db: Session = Depends(get_db)):
+    peers_from_db = db.query(models.Peer).all()
 
     return templates.TemplateResponse(
-        "dashboard.html", {"request": request, "title": "dashboard", "peers": peers_db}
+        "dashboard.html", {"request": request, "title": "dashboard", "peers": peers_from_db}
     )
 
 
--- app/wg/utils.py
+++ app/wg/utils.py
@@ -1,25 +1,15 @@
 from sqlalchemy.orm import Session
-from ..db import models
+from db import models
 
 def assign_ip(db: Session):
     all_peers = db.query(models.Peer).all()
+    
+    
+    assigned_ips = [peer.assigned_ip.split('/')[0] for peer in all_peers if peer.assigned_ip]
 
-    assigned_ips = [peers.assigned_ips for peers in all_peers if peers.assigned_ips]
-        for i in range(2, 255):
+    for i in range(2, 255):
         candidate_ip = f"10.0.0.{i}"
-        
         if candidate_ip not in assigned_ips:
             return candidate_ip
             
     return None
-
-
-
-
-    
-
-
-    
-    
-
-
